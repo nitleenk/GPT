b02fbb5ecdaeb36635aaf9d86d590a9f427c0b8c
Nitleen Kaur <127779292+nitleenk@users.noreply.github.com>
2023-07-23 18:02:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+!pip install openai
+!pip install gradio
+import openai
+import gradio
 
+
+openai.api_key="Your API key"
+
+messages = [{"role":"system","content": "You are a chatbot"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role":"user", "content": user_input})
+    response=openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    ChatGPT_reply=response["choices"][0]["message"]["content"]
+    messages.append({"role":"assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo=gradio.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="myGPT")
+
+demo.launch(share=True)
